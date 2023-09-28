fd65a8f41d5f6b5181848cafb4f6c8ef3c1e07c7
August7337 <augustin.tarit@gmail.com>
2023-09-28 19:30:10 +0200

    debug

--- NumworksGame.py
+++ NumworksGame.py
@@ -11,24 +11,30 @@ py=175
 sx=20
 sy=40
 
-Pvelocity = 10
+Pvelocity = 1
 
 def sq(xp,yp):
   fill_rect(xp,yp,
   sx,sy,
-  color(220,120,220))  
+  color(220,120,220))
+  draw_string('px', 5, 10)
+  draw_string(str(px), 30, 10)
+
+  draw_string('py', 5, 30)
+  draw_string(str(py), 30, 30)
+  
+a=False
 
 while i==0:
-    
   if keydown(KEY_RIGHT):
     fill_rect(px,py,Pvelocity,sy,color(255,255,255))
-    px=px+Pvelocity
+    px=px+Pvelocity  
     
-  if keydown(KEY_LEFT):
+  if keydown(KEY_LEFT and a):
     fill_rect(px+sx-Pvelocity,py,Pvelocity,sy,color(255,255,255))
     px=px-Pvelocity
     
-  if keydown(KEY_UP):
+  if keydown(KEY_UP and py>0):
     fill_rect(px,py+sy-Pvelocity,sx,Pvelocity,color(255,255,255))
     py=py-Pvelocity
     
